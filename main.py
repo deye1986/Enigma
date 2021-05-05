ae291d23c30be3e1252a0963cd66b2a1eb078067
David Ikin <shadow2272@aol.com>
2021-05-05 04:50:27 +0100

    Pep enhancements

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from math import trunc
 def enigma():
     '''cypher'''
     
-    message = input('INPUT MESSAGE TO BE CODED (CTRL + C to quit) \n >>> ')
+    message = input('INPUT MESSAGE TO BE CODED (CTRL + C to quit) - Works best one word at a time \n >>> ')
     if message == '':
         enigma()
     else:
@@ -21,7 +21,7 @@ def enigma():
         rotorI = [4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9]
         rotorII = [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]
         rotorIII = [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14]
-        reflektor = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
+        lamp = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
         plugboard = [0, 24, 2, 3, 4, 5, 22, 7, 8, 9, 10, 12, 11, 16, 14, 15, 13, 17, 18, 19, 20, 21, 6, 23, 1, 25]
         rotIstart = 7
         rotIIstart = 24
@@ -89,7 +89,7 @@ def enigma():
         seqI = rotor(rotorIM, 0, seq, 1)
         seqII = rotor(rotorIIM, 0, seqI, 2)
         seqIII = rotor(rotorIIIM, 0, seqII, 3) 
-        seqIV = refPlug(reflektor, seqIII)
+        seqIV = refPlug(lamp, seqIII)
         seqVI = rotor(rotorIIIM, 1, seqIV, 3)
         seqVII = rotor(rotorIIM, 1, seqVI, 2)
         seqVIII = rotor(rotorIM, 1, seqVII, 1)
@@ -105,3 +105,4 @@ def enigma():
 
 while(1):
     enigma()
+
